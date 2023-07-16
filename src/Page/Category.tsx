6f61b9b0bc1a5ce825fceede02340a6a79416307
unknown <mohammad.daaboul.99@gmail.com>
2023-07-16 11:53:51 +0300

    add src image

--- src/Page/Category.tsx
+++ src/Page/Category.tsx
@@ -2,6 +2,8 @@ import { Box, Container, Image, SimpleGrid } from '@chakra-ui/react';
 import { enableState, userInfo } from '../state/recoil_state';
 import { useState, useEffect } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
+import { photo } from '../photo';
+
 
 export default function Category() {
   const [user, setUser] = useRecoilState(userInfo);
@@ -26,17 +28,17 @@ export default function Category() {
       setUser({ ...user, interest: [...checkedItems, item] });
     }
 
-    
+
   };
 
   const title = ['Innovation', 'Medicine', 'Education', 'Pharmtech', 'Research', 'Technology', 'Telemedicine', 'Lorem Ipsum'];
 
   return (
-    <section  id="category" className="text-center h-[100%] mb-5">
+    <section id="category" className="text-center h-[100%] mb-5">
       <Container maxW="sm">
         <h1 className="text-[28px] text-[#434E61] font-bold">Tell us what you’re interested in</h1>
       </Container>
-      <SimpleGrid className="mt-5" columns={[2, 3, 4]}  spacing={5}>
+      <SimpleGrid className="mt-5" columns={[2, 3, 4]} spacing={5}>
         {title.map((item, index) => (
           <Box key={index} className="relative" width="138px">
             <input
@@ -54,7 +56,7 @@ export default function Category() {
               htmlFor={String(index)}
               className="inline-flex items-center justify-between rounded-[10px] cursor-pointer border-2 border-transparent peer-hover:border-[#FF8C1E] peer-hover:border-2  peer-checked:border-[#FF8C1E] peer-checked:border-2"
             >
-              <Image className="rounded-[10px]" boxSize="125px" src={`src/assets/category-${index + 1}.png`} alt="category" />
+              <Image className="rounded-[10px]" boxSize="125px" src={photo[index]} alt="category" />
               <p className="absolute top-[70%] left-[10%] text-white">{item}</p>
             </label>
           </Box>
---
+++ src/photo.ts
@@ -0,0 +1,12 @@
+//mock.js
+
+export const photo = [
+    "src/assets/category-1.png",
+    "src/assets/category-2.png",
+    "src/assets/category-3.png",
+    "src/assets/category-4.png",
+    "src/assets/category-5.png",
+    "src/assets/category-6.png",
+    "src/assets/category-7.png",
+    "src/assets/category-8.png",
+]
